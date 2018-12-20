ae7a6215eda1bc8ed99713f5693c744e3d3b1d96
wangyy <hi_liubai@163.com>
2018-12-20 10:42:20 +0800

    react脚手架

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <h1>hello<br />liu-xiaobai</h1>,
+  document.getElementById("root")
+);
+
+
